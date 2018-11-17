79dab39550fb7cbaad205ffb2fd6bf4887ea254d
Li Xiang <laoxing201314@outlook.com>
2018-11-17 23:51:07 +0800

    test

---
+++ doctest.py
@@ -0,0 +1,13 @@
+
+#import doctest
+
+#keyword
+import keyword
+print(keyword.kwlist)
+
+#Numbers
+a, b, c, d = 20, 5.5, True, 4+3j
+print(type(a), type(b), type(c), type(d))
+print(4 / 4)
+
+#String

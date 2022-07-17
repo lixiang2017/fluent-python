1c523d170d4f9bebf5b62b0dfcba4528bfe418fe
lixiang <838255715@qq.com>
2022-07-17 19:27:40 +0800

    fork example-code and example-code-2e

---
+++ test/test_literal.py
@@ -0,0 +1,15 @@
+
+from typing import Literal
+
+
+GenderValue = Literal[0, 1, 2, 9]
+
+print(GenderValue)
+print(type(GenderValue))
+
+
+'''
+typing.Literal[0, 1, 2, 9]
+<class 'typing._LiteralGenericAlias'>
+[Finished in 45ms]
+'''
---
+++ test/test_round.py
@@ -0,0 +1,49 @@
+
+
+for i in range(-5, 8):
+    print(f'i is {i}, round-of-i is {round(i)}')
+
+print('================================================')
+
+for i in range(-5, 15):
+    print(f'i+0.5 is {i + 0.5}, round-of-i+0.5 is {round(i + 0.5)}')
+
+
+
+'''
+i is -5, round-of-i is -5
+i is -4, round-of-i is -4
+i is -3, round-of-i is -3
+i is -2, round-of-i is -2
+i is -1, round-of-i is -1
+i is 0, round-of-i is 0
+i is 1, round-of-i is 1
+i is 2, round-of-i is 2
+i is 3, round-of-i is 3
+i is 4, round-of-i is 4
+i is 5, round-of-i is 5
+i is 6, round-of-i is 6
+i is 7, round-of-i is 7
+================================================
+i+0.5 is -4.5, round-of-i+0.5 is -4
+i+0.5 is -3.5, round-of-i+0.5 is -4
+i+0.5 is -2.5, round-of-i+0.5 is -2
+i+0.5 is -1.5, round-of-i+0.5 is -2
+i+0.5 is -0.5, round-of-i+0.5 is 0
+i+0.5 is 0.5, round-of-i+0.5 is 0
+i+0.5 is 1.5, round-of-i+0.5 is 2
+i+0.5 is 2.5, round-of-i+0.5 is 2
+i+0.5 is 3.5, round-of-i+0.5 is 4
+i+0.5 is 4.5, round-of-i+0.5 is 4
+i+0.5 is 5.5, round-of-i+0.5 is 6
+i+0.5 is 6.5, round-of-i+0.5 is 6
+i+0.5 is 7.5, round-of-i+0.5 is 8
+i+0.5 is 8.5, round-of-i+0.5 is 8
+i+0.5 is 9.5, round-of-i+0.5 is 10
+i+0.5 is 10.5, round-of-i+0.5 is 10
+i+0.5 is 11.5, round-of-i+0.5 is 12
+i+0.5 is 12.5, round-of-i+0.5 is 12
+i+0.5 is 13.5, round-of-i+0.5 is 14
+i+0.5 is 14.5, round-of-i+0.5 is 14
+[Finished in 44ms]
+'''
